98a0c52d35a896a0ba4195fc42e66bd0d10eadc7
mtimbone <67309759+mtimbone@users.noreply.github.com>
2021-02-16 15:30:45 -0600

    Final

--- lab3script.js
+++
@@ -1,9 +0,0 @@
-let milesDriven = prompt('How many miles did you drive?');
-let gallons = prompt('How many gallons of fuel does your car hold?');
-let milesPerGallon = milesDriven / gallons;
-document.write(`Your car gets ${milesPerGallon.toFixed(2)} miles per gallon.`);
-
-
-
-let person = prompt('Enter your name');
-alert(`Hello  ${person} Welcome to the JavaScript class!`);
