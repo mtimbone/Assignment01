f59b4c38bb197b1dcbeb14829c13b26618fef6f7
mtimbone <67309759+mtimbone@users.noreply.github.com>
2021-02-16 23:15:56 -0600

    Final

--- js/script.js
+++ js/script.js
@@ -1,183 +1,182 @@
 //STEP1
 
-let someMonth;
-function theMonth();
-let currentMonth;
-let summerMonth;
-let myLibraryFunction;
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
 
 //STEP2
 
 //numberic literal expression
 
-let mark = 10;
+// let mark = 10;
 
 //string literal expression
 
-let mark = "mark"
+// let mark = "mark"
 
 //boolean literal expression
 
-let mark = true;
+// let mark = true;
 
 //a null liteal expression
 
-let make = null;* /
+// let make = null;
 
 //STEP3
 
-let x = y + z;
-let mark = m + ark;
+// let x = y + z;
+// let mark = m + ark;
 
-let x = z;
-let x = y;
+// let x = z;
+// let x = y;
 
 
+//STEP4   
 
-//STEP4   Mark add Hungarian Notation
-
-let strFirstName;
-let strLastName;
-let strAddress;
-let strCity;
-let iZipCode;
-let iYourAge;
-let strReferralSource;
-let bMayWeContactYou;
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let iZipCode;
+// let iYourAge;
+// let strReferralSource;
+// let bMayWeContactYou;
 
 //STEP5
 
-let fistName = ("mark");
-let lastName = ("timbone")
-let address = ("sanDiego")
+// let fistName = ("mark");
+// let lastName = ("timbone")
+// let address = ("sanDiego")
 
 
 //STEP6
 
-let numStr = (17 + "str")
-console.log(numStr);
+// let numStr = (17 + "str")
+// console.log(numStr);
 
 
 //STEP7
 
-let boolStr = (true + "str")
-console.log(boolStr);
+// let boolStr = (true + "str")
+// console.log(boolStr);
 
-let numBool = (17 + true)
-console.log(numBool);
+// let numBool = (17 + true)
+// console.log(numBool);
 
 
 //STEP8
 
-The string literal was not valid because of the overuse use of quotation marks and missing brackets.
+// The string literal was not valid because of the overuse of quotation marks and missing brackets.
 
 
-let someString = (`Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.`)
-window.console.log(someString);
+// let someString = (`Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.`)
+// window.console.log(someString);
 
 //STEP9
 
-let some_item = null;
-console.log(some_item)
+// let some_item = null;
+// console.log(some_item)
 
-let item;
-console.log(item)
+// let item;
+// console.log(item)
 
 //STEP10
 
-typeof "name";
-typeof 3;
-typeof true;
-typeof [1, 2, 3];
-typeof hi;
+// typeof "name";
+// typeof 3;
+// typeof true;
+// typeof [1, 2, 3];
+// typeof hi;
 
 
 //STEP11
 
-let name = ("Hello Zak Ruvalcaba," + "welcome to the JavaScript class!");
-alert(name);
+// let name = ("Hello Zak Ruvalcaba," + "welcome to the JavaScript class!");
+// alert(name);
 
-let myName = ("Hello" + " Mark Timbone" + ", welcome to the JavaScript class!");
-alert(myName);
+// let myName = ("Hello" + " Mark Timbone" + ", welcome to the JavaScript class!");
+// alert(myName);
 
 
 //STEP12
 
-let name = ("Mark Timbone");
-let myName = ("Hello " + name + ", welcome to the JavaScript class!");
-alert(myName);
+// let name = ("Mark Timbone");
+// let myName = ("Hello " + name + ", welcome to the JavaScript class!");
+// alert(myName);
 
 
 //STEP13
 
-let course = ("JavaScript");
-let myName = ("Hello, Mark Timbone, welcome to the" + course + " class!");
-alert(myName);
+// let course = ("JavaScript");
+// let myName = ("Hello, Mark Timbone, welcome to the" + course + " class!");
+// alert(myName);
 
 //STEP14
 
-let zak = ("Hello Zak Ruvalcaba. \nWelcome to the JavaScript class!");
-alert(zak);
+// let zak = ("Hello Zak Ruvalcaba. \nWelcome to the JavaScript class!");
+// alert(zak);
 
-or
+// or
 
-let zak = (`Hello Zak Ruvalcava.
-Welcome to the JavaScript class!`);
-alert(zak);
+// let zak = (`Hello Zak Ruvalcava.
+// Welcome to the JavaScript class!`);
+// alert(zak);
 
 
 //STEP15
 
-let person = prompt('Enter your name');
-alert(`Hello  ${person} Welcome to the JavaScript class!`);
+// let person = prompt('Enter your name');
+// alert(`Hello  ${person} Welcome to the JavaScript class!`);
 
 
 //STEP16
 
-let nameOfClass = prompt('Enter the name of your class');
-alert(`Hello Mark Timbone. Welcome to the ${nameOfClass} class!`);
+// let nameOfClass = prompt('Enter the name of your class');
+// alert(`Hello Mark Timbone. Welcome to the ${nameOfClass} class!`);
 
 
 //STEP17
 
-let x = 10;
-let y = 20;
-console.log(x + y);
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
 
 
 //STEP18
 
-let x = 20;
-let y = (x + 20)
-console.log(y);
+// let x = 20;
+// let y = (x + 20)
+// console.log(y);
 
 
 
 //STEP19
 
-let x = 20;
-let y = (x * 5);
-console.log(y);
+// let x = 20;
+// let y = (x * 5);
+// console.log(y);
 
 //STEP20
 
-let x = (20 % 3);
-let y = (x / 1);
-console.log(y);
+// let x = (20 % 3);
+// let y = (x / 1);
+// console.log(y);
 
 
 //STEP21
 
-let x = 1;
-let y = 2;
-let z = (x + y);
-z = 3;
-console.log(z);
+// let x = 1;
+// let y = 2;
+// let z = (x + y);
+// z = 3;
+// console.log(z);
 
 
 //STEP22
 
-const a = 3;
-const b = -2;
-console.log(!(a > 0 || b > 0));
+// const a = 3;
+// const b = -2;
+// console.log(!(a > 0 || b > 0));
 
